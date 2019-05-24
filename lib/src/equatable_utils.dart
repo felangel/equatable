2248a1b695e092f7dc6b6fa8088ab7ed5d87870e
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2019-05-24 16:03:38 +0700

    improve "equals" function, add test for SimpleEquatable has collection variable.

--- lib/src/equatable_utils.dart
+++ lib/src/equatable_utils.dart
@@ -1,3 +1,5 @@
+import 'package:collection/collection.dart';
+
 int mapPropsToHashCode(List props) {
   int hashCode = 0;
 
@@ -10,16 +12,24 @@ int mapPropsToHashCode(List props) {
   return hashCode;
 }
 
+const DeepCollectionEquality _equality = DeepCollectionEquality();
+
 bool equals(List list1, List list2) {
   if (identical(list1, list2)) return true;
   if (list1 == null || list2 == null) return false;
   int length = list1.length;
   if (length != list2.length) return false;
+
   for (int i = 0; i < length; i++) {
-    if (list1[i] is List && list2[i] is List) {
-      if (!equals(list1[i] as List, list2[i] as List)) return false;
+    final unit1 = list1[i];
+    final unit2 = list2[i];
+
+    if (unit1?.runtimeType != unit2?.runtimeType) return false;
+
+    if (unit1 is Iterable || unit1 is List || unit1 is Map || unit1 is Set ) {
+      if (!_equality.equals(unit1, unit2)) return false;
     } else {
-      if (list1[i] != list2[i]) return false;
+      if (unit1 != unit2) return false;
     }
   }
   return true;
