04650812f9b76600b0167301b5ef1472ee6c0eb8
Felix Angelov <felangelov@gmail.com>
2021-02-19 17:30:09 -0600

    chore: remove unused test/main.dart

--- test/main.dart
+++
@@ -1,9 +0,0 @@
-import 'equatable_config_test.dart' as equatable_config_test;
-import 'equatable_mixin_test.dart' as equatable_mixin_test;
-import 'equatable_test.dart' as equatable_test;
-
-void main() {
-  equatable_config_test.main();
-  equatable_test.main();
-  equatable_mixin_test.main();
-}
