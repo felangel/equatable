725b76c9ef072695f3ae4f036c4fa5e015528f13
azack <423671+azack@users.noreply.github.com>
2023-10-26 22:04:23 -0400

    chore: fix typo in doc comment (#170)

--- lib/src/equatable_config.dart
+++ lib/src/equatable_config.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_classes_with_only_static_members
 import 'package:equatable/src/equatable.dart';
 
-/// The default configurion for all [Equatable] instances.
+/// The default configuration for all [Equatable] instances.
 ///
 /// Currently, this config class only supports setting a default value for
 /// [stringify].
