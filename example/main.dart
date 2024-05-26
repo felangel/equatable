47c1a01d390e697b6616a7d91b5b09daa330d2e6
Felix Angelov <felangelov@gmail.com>
2024-05-26 14:57:30 -0500

    chore: simplify example

--- example/main.dart
+++ example/main.dart
@@ -2,45 +2,12 @@
 import 'package:equatable/equatable.dart';
 
 @Equatable()
-class Credentials {
-  const Credentials({required this.username, required this.password});
-
-  final String username;
-  final String password;
-}
-
-@Equatable()
-class EquatableDateTime extends DateTime {
-  EquatableDateTime(
-    super.year, [
-    super.month,
-    super.day,
-    super.hour,
-    super.minute,
-    super.second,
-    super.millisecond,
-    super.microsecond,
-  ]);
+class Person {
+  Person({required this.name});
+  final String name;
 }
 
 void main() {
-  const credentialsA = Credentials(username: 'Joe', password: 'password123');
-  const credentialsB = Credentials(username: 'Bob', password: 'password!');
-  const credentialsC = Credentials(username: 'Bob', password: 'password!');
-
-  print(credentialsA == credentialsA); // true
-  print(credentialsB == credentialsB); // true
-  print(credentialsC == credentialsC); // true
-  print(credentialsA == credentialsB); // false
-  print(credentialsB == credentialsC); // true
-
-  final dateTimeA = EquatableDateTime(2019);
-  final dateTimeB = EquatableDateTime(2019, 2, 20, 19, 46);
-  final dateTimeC = EquatableDateTime(2019, 2, 20, 19, 46);
-
-  print(dateTimeA == dateTimeA); // true
-  print(dateTimeB == dateTimeB); // true
-  print(dateTimeC == dateTimeC); // true
-  print(dateTimeA == dateTimeB); // false
-  print(dateTimeB == dateTimeC); // true
+  print(Person(name: 'Dash') == Person(name: 'Dash')); // true
+  print(Person(name: 'Dash') == Person(name: 'Sparky')); // false
 }
