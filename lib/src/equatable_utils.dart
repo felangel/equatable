9981799c16f75712cbc282330f6274be4033a9f5
Muhammad Kamel <mu7ammadkamel@hotmail.com>
2026-01-05 19:20:14 +0200

    revert: keep Jenkins hash algorithm instead of Object.hashAll.

--- lib/src/equatable_utils.dart
+++ lib/src/equatable_utils.dart
@@ -3,30 +3,7 @@ import 'package:equatable/equatable.dart';
 
 /// Returns a `hashCode` for [props].
 int mapPropsToHashCode(Iterable<Object?>? props) {
-  if (props == null) return 0;
-  return Object.hashAll(props.map(_computeHashCode));
-}
-
-int _computeHashCode(Object? object) {
-  if (object is Map) {
-    final entries = object.entries
-        .sorted((a, b) => a.key.hashCode.compareTo(b.key.hashCode));
-    return Object.hashAll(
-      entries
-          .expand((e) => [_computeHashCode(e.key), _computeHashCode(e.value)]),
-    );
-  }
-  if (object is Set) {
-    return Object.hashAll(
-      object
-          .sorted((a, b) => a.hashCode.compareTo(b.hashCode))
-          .map(_computeHashCode),
-    );
-  }
-  if (object is Iterable) {
-    return Object.hashAll(object.map(_computeHashCode));
-  }
-  return object.hashCode;
+  return _finish(props == null ? 0 : props.fold(0, _combine));
 }
 
 /// Determines whether two lists ([a] and [b]) are equal.
@@ -102,6 +79,38 @@ bool objectsEquals(Object? a, Object? b) {
 @pragma('vm:prefer-inline')
 bool _isEquatable(Object? object) {
   return object is Equatable || object is EquatableMixin;
+}
+
+/// Jenkins Hash Functions
+/// https://en.wikipedia.org/wiki/Jenkins_hash_function
+int _combine(int hash, Object? object) {
+  if (object is Map) {
+    object.keys
+        .sorted((Object? a, Object? b) => a.hashCode - b.hashCode)
+        .forEach((Object? key) {
+      hash = hash ^ _combine(hash, [key, (object! as Map)[key]]);
+    });
+    return hash;
+  }
+  if (object is Set) {
+    object = object.sorted((Object? a, Object? b) => a.hashCode - b.hashCode);
+  }
+  if (object is Iterable) {
+    for (final value in object) {
+      hash = hash ^ _combine(hash, value);
+    }
+    return hash ^ object.length;
+  }
+
+  hash = 0x1fffffff & (hash + object.hashCode);
+  hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+  return hash ^ (hash >> 6);
+}
+
+int _finish(int hash) {
+  hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+  hash = hash ^ (hash >> 11);
+  return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
 }
 
 /// Returns a string for [props].
